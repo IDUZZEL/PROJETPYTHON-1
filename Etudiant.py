7d3211ad439c76c7cfc41ac82c2082c5786a4b9e
Yassir Sbai <63135823+IDUZZEL@users.noreply.github.com>
2023-11-18 14:58:12 +0100

    Update Etudiant.py

--- Etudiant.py
+++ Etudiant.py
@@ -1,8 +1,11 @@
 class Etudiant:
-    def __init__(self, id, nom, prenom, email, login, mdp):
-        self.id = id
-        self.nom = nom
-        self.prenom = prenom
-        self.email = email
-        self.login = login
-        self.mdp = mdp
+	def __init__(self,id, nom, prenom, email, login ,password,suspended=False):
+		self.id = id
+		self.nom = nom
+		self.prenom = prenom
+		self.email = email
+		self.login = login
+		self.password = password
+		self.suspended = suspended
+	def __str__(self):
+		return f"{self.id},{self.nom},{self.prenom},{self.email},{self.login},{self.password},{self.suspended} \n"
