8904f6f30aea575e9dc60e2b75f9ff78197d69ec
Yassir Sbai <63135823+IDUZZEL@users.noreply.github.com>
2023-11-18 14:57:47 +0100

    Update Administrateur.py

--- Administrateur.py
+++ Administrateur.py
@@ -1,4 +1,5 @@
 import Livre
+from prettytable import PrettyTable
 class Administrateur:
     def __init__(self ,id, nom, prenom, email, login, mdp):
         self.id=id
@@ -7,3 +8,24 @@ class Administrateur:
         self.email = email
         self.login = login
         self.mdp = mdp
+def ajoutEtudiant():
+	file = open("users.txt","a+")
+	id_input = input("id: ")
+	nom_input = input("nom: ")
+	prenom_input = input("prenom: ")
+	email_input = input("email: ")
+	login_input = input("login: ")
+	password_input = input("password: ")
+	x = Etudiant(id_input, nom_input, prenom_input, email_input, login_input, password_input)
+	print(x.__str__())
+	file.write(x.__str__())
+	file.close()
+
+def afficheEtudiant():
+	file = open("users.txt","r")
+	table = PrettyTable(["id","nom","prenom","email","login","password","suspended"])
+	for line in file.readlines():
+		etudiant=line.strip().split(',')
+		table.add_row([etudiant[0], etudiant[1], etudiant[2],etudiant[3], etudiant[4], etudiant[5],etudiant[6]])
+	print(table)
+	file.close()
