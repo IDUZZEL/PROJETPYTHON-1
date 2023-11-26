8987a90e7d571d81218bff6f8ab63c84c704922d
Yassir Sbai <63135823+IDUZZEL@users.noreply.github.com>
2023-11-26 21:48:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import Etudiant
+import Administrateur as Admin
+MenuChoix = input("""Saisissez votre choix 1/2 \n
+                  1_ Login en tant qu'administrateur
+                  2_ Login en tant qu'étudiant\n""")
+
+match MenuChoix:
+    case '1':
+        Admin.AdminMain()
+    case '2':
+        Etudiant.EtudiantMain()
+    case _:
+        print("Choix introuvable ...")
 
